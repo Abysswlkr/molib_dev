1a0d1cbdd313577b6bcdad1c7c3551872f62e000
Miguel Olivares <miguel.olivaresbueno@outlook.com>
2024-06-01 18:20:38 -0400

    update buttons, loading

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,17 @@
+//Loader
+window.addEventListener('load', function () {
+  // Ensure the preloader is shown for at least 10 seconds
+  setTimeout(function () {
+      const preloader = document.getElementById('preloader');
+      preloader.style.display = 'none';
+
+      const content = document.getElementById('content');
+      content.classList.remove('hidden');
+      content.classList.add('visible');
+  }, 2000); 
+});
+
+
 //solanum text effect entrance
 document.addEventListener('DOMContentLoaded', function() {
   const text = '"Podríamos no tener mucho en común, tú y yo. Aun así, te considero un amigo"';
@@ -27,120 +41,19 @@ document.addEventListener('DOMContentLoaded', function() {
   observer.observe(document.querySelector('.container-solanum'));
 });
 
-//Particles.JS library
-particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 150,
-        "density": {
-          "enable": true,
-          "value_area": 1300,
-        }
-      },
-      "color": {
-        "value": "#191919"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 3,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 1,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "grab"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 140,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true
+
+  //Redirections
+  document.getElementById('redirect-github').addEventListener('click', function() {
+    window.open('https://github.com/Abysswlkr', '_blank');
+  });
+  document.getElementById('redirect-linkedin').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/molivaresb/', '_blank');
+  });
+  document.getElementById('redirect-x').addEventListener('click', function() {
+    window.open('https://twitter.com/Abysswlkr_', '_blank');
   });
 
 
-  //Redirections
   document.getElementById('link1').addEventListener('click', function() {
       window.open('https://github.com/Abysswlkr/Crvice_Project', '_blank');
   });
@@ -164,6 +77,23 @@ particlesJS("particles-js", {
   document.getElementById('link6').addEventListener('click', function() {
     window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
   });
+
+  //Download CV
+  function downloadCV() {
+    const url = 'assets/pdf/OLIVARES-BUENO-MIGUEL-CV.pdf';
+    
+    const enlace = document.createElement('a');
+    enlace.href = url;
+    
+    enlace.setAttribute('download', 'OLIVARES-BUENO-MIGUEL-CV.pdf');
+    
+    enlace.style.display = 'none';
+    document.body.appendChild(enlace);
+    enlace.click();
+    document.body.removeChild(enlace);
+}
+
+
 
 
 
